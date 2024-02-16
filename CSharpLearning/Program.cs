51d6e360d79d8cc61fdc5f785200fdaf998b8f89
masoud ahmadi <masoud.ahmadi@estdco.com>
2024-02-16 11:13:20 +0330

    In this tutorial, we will only use WriteLine() as it makes it easier to read the output of code.

--- CSharpLearning/Program.cs
+++ CSharpLearning/Program.cs
@@ -14,6 +14,7 @@ namespace CSharpLearning
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            #region Comment
             // single line comment
 
 
@@ -21,7 +22,24 @@ namespace CSharpLearning
              * multiline
              * comment
              */
-           
+
+            #endregion
+
+            #region C# Output
+            // To output values or print text in C#, you can use the WriteLine() method:
+
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("I am Learning C#");
+            //Console.WriteLine("It is awesome!");
+
+            // You can also output numbers, and perform mathematical calculations:
+            //Console.WriteLine(3 + 3);
+
+            #endregion
+
+
+
+            Console.ReadKey();
         }
     }
 }
