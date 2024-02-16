50bc028cdbed996bd5b6e00829b4567f6bb34f7d
masoud ahmadi <masoud.ahmadi@estdco.com>
2024-02-16 11:19:35 +0330

    explains c sharp variables.

--- CSharpLearning/Program.cs
+++ CSharpLearning/Program.cs
@@ -37,9 +37,39 @@ namespace CSharpLearning
 
             #endregion
 
+            #region  C# Variables
+            /*  Variables are containers for storing data values.
+             *  In C#, there are different types of variables (defined with different keywords), for example:
+             *  int - stores integers (whole numbers), without decimals, such as 123 or -123
+             *  double - stores floating point numbers, with decimals, such as 19.99 or -19.99
+             *  char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+             *  string - stores text, such as "Hello World". String values are surrounded by double quotes
+             *   bool - stores values with two states: true or false
+            */
 
+            //// Declaring(Creating) Variables
+            //// type variableName = value;
+            //string name = "John";
+            //Console.WriteLine(name);
+
+            //int myNum = 15;
+            //Console.WriteLine(myNum);
+
+            //// You can also declare a variable without assigning the value, and assign the value later:
+            //int myNum;
+            //myNum = 15;
+            //Console.WriteLine(myNum);
+
+            //// Examples
+            //int myNum = 5;
+            //double myDoubleNum = 5.99D;
+            //char myLetter = 'D';
+            //bool myBool = true;
+            //string myText = "Hello";
+
+            #endregion
 
             Console.ReadKey();
-        }
-    }
+}
+}
 }
