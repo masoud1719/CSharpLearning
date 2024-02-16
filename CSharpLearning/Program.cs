d5afba260e583a27fee6bcc7daebd0699f88110d
masoud ahmadi <masoud.ahmadi@estdco.com>
2024-02-16 11:27:39 +0330

    explain constant variables and identifiers.

--- CSharpLearning/Program.cs
+++ CSharpLearning/Program.cs
@@ -66,10 +66,42 @@ namespace CSharpLearning
             //char myLetter = 'D';
             //bool myBool = true;
             //string myText = "Hello";
+            #region Constant Variables
+            /*
+             * If you don't want others (or yourself) to overwrite existing values, you can add the const keyword in front of the variable type.
+             * An example that is often referred to as a constant, is PI (3.14159...).
+             */
+            //const int myNum = 15;
+            //myNum = 20; // error (visual studio will show the error before run the program as well.)
 
             #endregion
+            #region Identifier
+            /*
+                *All C# variables must be identified with unique names.
+                *These unique names are called identifiers.
+                *Identifiers can be short names (like x and y) or more descriptive names (age, sum, totalVolume).
+                *Note: It is recommended to use descriptive names in order to create understandable and maintainable code:
+            */
+            //// Good
+            //int minutesPerHour = 60;
+
+            //// OK, but not so easy to understand what m actually is
+            //int m = 60;
+
+            /*
+                *The general rules for naming variables are:
+                *Names can contain letters, digits and the underscore character (_)
+                *Names must begin with a letter or underscore
+                *Names should start with a lowercase letter, and cannot contain whitespace
+                *Names are case-sensitive ("myVar" and "myvar" are different variables)
+                *Reserved words (like C# keywords, such as int or double) cannot be used as names
+            */
+
+            #endregion
+            #endregion
+
 
             Console.ReadKey();
-}
-}
+        }
+    }
 }
